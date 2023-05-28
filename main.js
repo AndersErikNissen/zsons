98d97120c160db2c524c099b11a11aea1e95e939
Anders <1086088@ucn.dk>
2023-05-28 23:41:55 +0200

    Handling the data from <form>

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ class Graph extends HTMLElement {
   
   connectedCallback() {
     this._addContent();
+    this._initData();
     this.getData();
+
+    console.log(this.data)
   }
 
   _style() {
@@ -45,10 +48,68 @@ class Graph extends HTMLElement {
     content.innerHTML = "<span>This is some NICE, content!</span>"
   }
 
+
+  /*
+    Layouts
+    - Graph
+    - Bar chart
+      - Stacked on top of each other
+      - Stacked beside each other, grouped for each number (So "events", would all be beside each other, then "meetings" and so on)
+    - Backlog: Pie chart
+    - Difference between two array (Like LoL gold difference)
+
+    The data comes from and array of inputs [[name,value]...]
+
+    We want to end up with
+      An array of objects with:
+      - Name
+      - All the values
+
+      Global data:
+      - The highest number and the ones in between
+
+    Check for changes to current inputs or new ones with "slotchanged".
+
+  */
+
+  _initData() {
+    var handleArray = (entry, arr) => {
+      var name = entry[0];
+      var value = entry[1];
+      var i = arr.findIndex( entryObj => entryObj.name === name);
+      var obj = i === -1 
+      ? {
+          name: name,
+          label: name.charAt(0).toUpperCase() + name.slice(1),
+          values: []
+        }
+      : arr[i];
+
+      if(!isNaN(Number(value))) obj.values.push(Number(value));
+      if (i === -1) arr.push(obj);
+    }
+
+    var arr = [];
+    var form = this.shadowRoot.querySelector('slot[name="data"]').assignedNodes()[0];
+    if (form.tagName !== 'FORM') return;
+    form = new FormData(form);
+
+    [...form.entries()].forEach(entry => handleArray(entry, arr));
+
+
+    this.data = arr;
+  }
+
   getData() {
-    var data = this.shadowRoot.querySelector('slot[name="data"]').assignedNodes()[0];
-    var form = new FormData(data);
-    console.log(new URLSearchParams(form).toString())
+    
+    var form = new FormData();
+    console.log(...form.entries())
+    var obj = {};
+    var arr = [];
+
+    [...form.entries()].forEach( entry => obj[entry[0]] = entry[1]);
+
+    //console.log(new URLSearchParams(form).toString())
 
   }
 }
